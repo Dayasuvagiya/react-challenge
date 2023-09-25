7c930186344bae0f7f739b0f23b8b9fb5ee1e7be
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-09-25 14:20:30 +0200

    ComponentDidUpdate method

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,8 @@ import css from './App.module.css';
 // import RenderingLists from './components/RenderingLists';
 // import Sidebar from "./components/Sidebar";
 // import NavBarForm from "./components/NavBarForm";
-import LifeCyclesCDM from './components/LifeCyclesCDM';
+// import LifeCyclesCDM from './components/LifeCyclesCDM';
+import LifeCyclesCDU from './components/LifeCyclesCDU';
 
 
 function App() {
@@ -11,8 +12,10 @@ function App() {
       {/* Add your components here */}
      {/* <Sidebar />
       <NavBarForm /> 
-      <RenderingLists /> */}
-      <LifeCyclesCDM />
+      <RenderingLists /> 
+      <LifeCyclesCDM /> */}
+
+      <LifeCyclesCDU />
       
     </div>
   );

8198039b16afd9a70ceb2d0416c931cdf8f9d300
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-10-05 07:21:51 +0200

    Conditional effect

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -4,7 +4,7 @@ function UseEffectCounter() {
   const [count, setCount] = useState(0);
   useEffect(() => {
     document.title = count;
-  });
+  }, [count]);
   return (
     <div>
       <button onClick={
@@ -17,3 +17,5 @@ function UseEffectCounter() {
 }
 
 export default UseEffectCounter;
+// [count] apelu che A jyare 2 button apela hoy tyare jo Ak par click kariye to banne sathe perform na thay Ana mate
+// count A batave che aa tyarej fuction use thavu joye jyare aa render thay
